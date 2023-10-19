db289a8df1c72eb2ee54d9f421f21abafd2e11f3
yhoungdev <yhoungdev@gmail.com>
2023-10-19 21:14:27 +0100

    feat: design interface for P2P marketplace

---
+++ src/components/molecules/dashboard/marketplace/swapBox.tsx
@@ -0,0 +1,80 @@
+"use client";
+import React, { Fragment } from "react";
+import { Box, Badge, Text, Flex, Avatar, Button } from "@chakra-ui/react";
+import { FaEye } from "react-icons/fa";
+
+const BookShowcaseBox = () => {
+  return (
+    <Box
+      //   h={["450.242px"]}
+      borderRadius={"10px"}
+      w={["90%", "90%", "250px"]}
+      bg={"#212121"}
+      p={"1em"}
+      mx={["auto", "", "0"]}>
+      <Box
+        borderRadius={"8px"}
+        px=".7em"
+        h={"201.134px"}
+        w={"100%"}
+        bg={"black"}>
+        <Flex justifyContent="flex-end" alignItems={"end"}>
+          <Badge
+            my={"1em"}
+            px={"0.6em"}
+            py={"0.1em"}
+            bg={"brand.primary"}
+            color={"#fff"}
+            borderRadius={"md"}>
+            Fiction
+          </Badge>
+        </Flex>
+      </Box>
+
+      <Box my=".5em">
+        <Text
+          fontSize={["18px"]}
+          fontWeight={600}
+          color={"rgba(255, 255, 255, 0.70)"}>
+          The Crying Book
+        </Text>
+
+        <Text color={"brand.grayed"}>Lieather Christel</Text>
+      </Box>
+
+      <Flex my={"1em"} gap=".5em" alignItems="center">
+        <Avatar size={"sm"} />
+        <Box>
+          <Text>Obiabo </Text>
+          {/* <Badge
+            my={"1em"}
+            px={"0.6em"}
+            py={"0.1em"}
+            bg={"brand.primary"}
+            color={"#fff"}
+            borderRadius={"md"}>
+            Fiction
+          </Badge> */}
+        </Box>
+      </Flex>
+
+      <Flex gap=".5em" alignItems={"center"}>
+        <Button w={"100%"} bg={"brand.primary"}>
+          Send Request
+        </Button>
+        <Box
+          bg={"dark.40"}
+          borderRadius={"md"}
+          height="fit-content"
+          py=".8em"
+          px={"1em"}
+          width={"fit-content"}
+          cursor="pointer">
+          <FaEye />
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default BookShowcaseBox;
---
+++ src/components/pages/dashbord/marketplace/swap.tsx
@@ -0,0 +1,12 @@
+import BookShowcaseBox from "@/components/molecules/dashboard/marketplace/swapBox";
+import React from "react";
+
+const MarketplaceSwap = () => {
+  return (
+    <div>
+      <BookShowcaseBox />
+    </div>
+  );
+};
+
+export default MarketplaceSwap;
