16bd491426409b9118b164b2344eec0d2f7a4e25
yhoungdev <yhoungdev@gmail.com>
2023-10-19 13:24:13 +0100

    ehn" updated loading state

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,64 +1,9 @@
-// //@ts-nocheck
-// import ChakraWrapper from "@/provider/chakraProvider";
-// import { Inter } from "next/font/google";
-// import "@/styles/main.scss";
-// import { Poppins } from "next/font/google";
-// import { Analytics } from "@vercel/analytics/react";
-// const inter = Inter({ subsets: ["latin"] });
-
-// const poppins = Poppins({
-//   subsets: ["latin"],
-//   weight: ["400", "500", "600", "700", "800"],
-// });
-
-// export const metadata = {
-//   title: "Bookhive",
-//   description: "An Open Platform to Engage Book Lovers.",
-//   icons: {
-//     icon: {
-//       url: "/logo.png",
-//       type: "image/ico",
-//     },
-//     shortcut: { url: "/logo.png", type: "image/png" },
-//   },
-//   openGraph: {
-//     title: "Bookhive",
-//     description: "An Open Platform to Engage Book Lovers ( Bibliophiles ).",
-//   },
-// };
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <html lang="en">
-//       <head>
-//         <meta charSet="UTF-8" />
-//         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-//         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-//         {/* <title>Document</title> */}
-//       </head>
-//       <body className="poppins.className">
-//         <body>
-//           <ChakraWrapper>{children}</ChakraWrapper>
-//           <Analytics />
-//         </body>
-//       </body>
-//     </html>
-//   );
-// }
-
-"use client";
+//@ts-nocheck
 import ChakraWrapper from "@/provider/chakraProvider";
 import { Inter } from "next/font/google";
 import "@/styles/main.scss";
 import { Poppins } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
-import { useEffect, useState } from "react";
-import Preloader from "@/components/preloader/Preloader";
-
 const inter = Inter({ subsets: ["latin"] });
 
 const poppins = Poppins({
@@ -78,7 +23,7 @@ export const metadata = {
   },
   openGraph: {
     title: "Bookhive",
-    description: "An Open Platform to Engage Book Lovers (Bibliophiles).",
+    description: "An Open Platform to Engage Book Lovers ( Bibliophiles ).",
   },
 };
 
@@ -87,28 +32,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [loading, setLoading] = useState(true);
-
-  // useEffect(() => {
-  //   if (typeof window !== "undefined") {
-  //     window.addEventListener("load", () => {
-  //       setLoading(false);
-  //     });
-
-  //     return () => {
-  //       window.removeEventListener("load", () => {
-  //         setLoading(false);
-  //       });
-  //     };
-  //   }
-  // }, []);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 500);
-  }, []);
-
   return (
     <html lang="en">
       <head>
@@ -118,8 +41,10 @@ export default function RootLayout({
         {/* <title>Document</title> */}
       </head>
       <body className="poppins.className">
-        {loading ? <Preloader /> : <ChakraWrapper>{children}</ChakraWrapper>}
-        <Analytics />
+        <body>
+          <ChakraWrapper>{children}</ChakraWrapper>
+          <Analytics />
+        </body>
       </body>
     </html>
   );
