358d695ce1546b5d4be45a2e41292242e6ab90cd
yhoungdev <yhoungdev@gmail.com>
2023-10-31 07:17:02 +0100

    chore: added copy to clip board

---
+++ src/utils/copyToClipboard.ts
@@ -0,0 +1,10 @@
+export const copyToClipboard = (data: string) => {
+    const isAble = navigator?.clipboard?.writeText;
+    //@ts-ignore
+        if (isAble) {
+            navigator?.clipboard.writeText(data);
+        } else {
+            console.log('failed to copy')
+        }
+
+};
