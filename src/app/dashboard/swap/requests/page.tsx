3cb8276b3703d7274f031343910d0b5dc4dc6c87
yhoungdev <yhoungdev@gmail.com>
2023-11-15 10:23:31 +0100

    chore: added swap request page

--- src/app/dashboard/swap/requests/page.tsx
+++ src/app/dashboard/swap/requests/page.tsx
@@ -1,11 +1,37 @@
 import React from "react";
 import { Flex, Text, Box } from "@chakra-ui/react";
+import { AUTH_COOKIE } from "@/constants";
+import { axiosInstance } from "@/utils/axios";
+import { useQuery } from "@tanstack/react-query";
+
 const Page = () => {
+  const getData = async () => {
+    const response = await axiosInstance("/marketplace/get_all_swap_listings", {
+      headers: {
+        Authorization: `Bearer ${AUTH_COOKIE}`,
+      },
+    });
+    return response.data;
+  };
+
+  // Use the useQuery hook
+  const { data, isError, isLoading, error } = useQuery({
+    queryKey: ["swap_request"],
+    queryFn: getData,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
   return (
     <div>
       <Flex>
-        <Text fontSize={["16px"]} fontWeight={"medium"}>
-          Requests
+        <Text fontSize={["20px"]} fontWeight={"bold"}>
+          All Requests
         </Text>
       </Flex>
 
